38f863883eda9bd6aeba7de8c2df15ce6f703f4c
mstrout <mstrout@bf4f0a70-a703-0410-be52-a7d65f7d3716>
2006-03-27 21:56:30 +0000

    Copied DuffsDevice to correct directory.

---
+++ trunk/TestCases/CFGtests/DuffsDevice.c
@@ -0,0 +1,19 @@
+void main () {
+
+  int count, n;
+  char *to, *from;
+
+  switch (count % 8)
+  {
+   case 0:        do {  *to = *from++;
+   case 7:              *to = *from++;
+   case 6:              *to = *from++;
+   case 5:              *to = *from++;
+   case 4:              *to = *from++;
+   case 3:              *to = *from++;
+   case 2:              *to = *from++;
+   case 1:              *to = *from++;
+                      } while (--n > 0);
+  }
+
+}
