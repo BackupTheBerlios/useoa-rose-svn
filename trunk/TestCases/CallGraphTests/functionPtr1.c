f92d555ab6b11156d0fdcb0b83a856a698291ddd
kreaseck <kreaseck@bf4f0a70-a703-0410-be52-a7d65f7d3716>
2006-07-10 15:19:15 +0000

    7/10/06 UseOA-Rose svn tag trunk r107 Barbara Kreaseck 	- added/editted some test cases for CallGraph

--- trunk/TestCases/CallGraphTests/functionPtr1.c
+++ trunk/TestCases/CallGraphTests/functionPtr1.c
@@ -1,21 +1,24 @@
-/* functionPtr2.c */
+/* functionPtr1.c */
 
   int read(); 
 
   int (*fp)();      // declaration of a function pointer 
   int foo();        // prototype for function foo 
   int bar();        // prototype for function bar 
+  int zoo();        // prototype for function bar 
 
   void main() { 
     int x = read(); 
-
+    fp = zoo;
     if (x) { 
       fp = foo; 
-    } else { 
-      fp = bar; 
     } 
+//    else { 
+//      fp = bar; 
+//    } 
     fp(); 
   } 
 
   int foo() {}
   int bar() {}
+  int zoo() {}
