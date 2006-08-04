479a7320825226c26f516c62aee0bdf750e35943
mstrout <mstrout@bf4f0a70-a703-0410-be52-a7d65f7d3716>
2006-08-04 22:08:39 +0000

    Adding another test case to test AliasIRInterface.

---
+++ trunk/TestCases/AliasTests/assignOps.c
@@ -0,0 +1,16 @@
+/*
+  Testing MemRefExprs for some of the SomethingAssignOps.
+  For example, SgPlusAssignOp, SgAndAssignOp, etc.
+*/
+
+int main() {
+    int a, b;
+    a = 20;
+
+    a++;
+
+    a += 45;
+
+    return a = b -= a;
+
+}
