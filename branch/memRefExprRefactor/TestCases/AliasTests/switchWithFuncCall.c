04af82fcdbb813fb033188bf0143cd8f3efbb62c
mstrout <mstrout@bf4f0a70-a703-0410-be52-a7d65f7d3716>
2006-09-13 23:06:53 +0000

    memRefExprRefactor branch - Generating a dot file for the bug where we don't get a proper call MRE   when the function call is in a case statement.

---
+++ branch/memRefExprRefactor/TestCases/AliasTests/switchWithFuncCall.c
@@ -0,0 +1,24 @@
+int bar() {
+    return 0;
+}
+
+float foo(char op)
+{
+  float x=2.;
+  float y=3.;
+  float z;
+  
+  switch(op)
+  {
+    case '+':
+      bar();
+      break;
+    
+    default:
+      bar();
+      break;      
+  }
+   return z;
+}
+
+
