916a081735eaa1cedc8923edd5956e887a170a6b
malusare <malusare@bf4f0a70-a703-0410-be52-a7d65f7d3716>
2007-04-11 22:01:40 +0000

    Added TestCase simulating conditional statement in 'C' language for ICFGActive TestCase.

--- trunk/TestCases/ICFGActive/conditional.c
+++ trunk/TestCases/ICFGActive/conditional.c
@@ -3,12 +3,13 @@
 int main()
 {
    int a,b,c;
-#pragma $adic_indep a
+#pragma $adic_indep b
+#pragma $adic_indep c
 
      a = 10;
  
      a = ( (a<=0) ? b : c );
 
-#pragma $adic_dep c
+#pragma $adic_dep a
 
 }
